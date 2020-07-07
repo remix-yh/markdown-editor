4f7ad8e88645872df56a054d1e0910acfdc63f62
remix.re.yh <remix.re.yh@gmail.com>
2020-07-07 22:59:11 +0900

    Markdigのサンプルコード追加

---
+++ MarkdownEditor/MarkdownParseLib/MarkdownParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Markdig;
+
+namespace MarkdownParseLib
+{
+    public class MarkdownParser
+    {
+        public static string ParseHtml(string markdown)
+        {
+            return Markdown.ToHtml(markdown);
+        }
+    }
+}
